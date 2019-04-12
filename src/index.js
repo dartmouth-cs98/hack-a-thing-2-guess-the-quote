f838dad483b7523492709eb23423301aae3fc40b
Rohan Bose <rohan.a.bose.20@dartmouth.edu>
2019-04-11 20:44:44 -0400

    Surge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+// import { strict } from 'assert';
+
+let str = '';
+let num = 1;
+
+setInterval(() => {
+  str = `You've been on this page for ${num} seconds`;
+  $('#main').html(str);
+  num += 1;
+}, 1000);
