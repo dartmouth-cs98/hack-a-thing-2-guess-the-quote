1a515ecb1795aabb1850b157fe00fa399fc78dbf
Rohan Bose <rohan.a.bose.20@dartmouth.edu>
2019-04-15 12:30:39 -0400

    Start of React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
-// import { strict } from 'assert';
 
-let str = '';
-let num = 1;
+// let str = '';
+// let num = 1;
 
-setInterval(() => {
-  str = `You've been on this page for ${num} seconds`;
-  $('#main').html(str);
-  num += 1;
-}, 1000);
+// setInterval(() => {
+//   str = `You've been on this page for ${num} seconds`;
+//   $('#main').html(str);
+//   num += 1;
+// }, 1000);
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
