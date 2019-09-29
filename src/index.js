bd901f077b4f7a206a073e9e52074bda51d2e114
Rohan Bose <rohanbose@Rohans-MacBook-Air.local>
2019-09-29 15:08:08 -0400

    starterpack

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
 
-// let str = '';
-// let num = 1;
+let str = '';
+let num = 1;
 
-// setInterval(() => {
-//   str = `You've been on this page for ${num} seconds`;
-//   $('#main').html(str);
-//   num += 1;
-// }, 1000);
+setInterval(() => {
+  str = `You've been on this page for ${num} seconds`;
+  $('#main').html(str);
+  num += 1;
+}, 1000);
 
 const App = () => <div className="test">All the REACT are belong to us!</div>;
 
