2b39be7276a632c6b288917693f2876bf8330596
Shoshana <Shoshana.M.Geller.20@dartmouth.edu>
2019-09-30 22:26:26 -0400

    turned api calls into a game

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './style.scss';
 
 import axios from 'axios';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import $ from 'jquery';
+import reducers from './reducers';
+
+
+// const App = () => <div className="test">Hey</div>;
+
+import App from './components/app';
+
 let str = '';
 let num = 1;
 // let num = 1;
@@ -15,35 +26,50 @@ let num = 1;
 //   $('#main').html(str);
 // });
 
-num = Math.floor(Math.random() * 2 + 1)
+$('.logo').attr('src', 'src/img/ron.jpg');
+$('.logo').attr('width', '200px');
+$('.logo2').attr('src', 'src/img/kanye.jpg');
+$('.logo2').attr('height', '300px');
+$('#main2').text('Click the one you think! The real speaker will remain.');
 
+num = Math.floor(Math.random() * 2 + 1);
 
 if (num === 1) {
-    axios.get('https://api.kanye.rest').then((response) => {
+  axios.get('https://api.kanye.rest').then((response) => {
     str = JSON.stringify(response.data.quote);
+    console.log(str);
     $('#main').text(str);
-    });
+  });
 } else {
-    axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes').then((response) => {
-    str = JSON.stringify(response.data);
-    $('#main').text(str);
-    });
-}
-axios.get('https://api.tronalddump.io/random/quote', {headers: {'accept': 'application/hal+json'}}).then((response) => {
+  axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes').then((response) => {
     str = JSON.stringify(response.data);
     console.log(str);
-    $('#main2').text(str);
-    });
+    $('#main').text(str);
+  });
+}
 
-const App = () => <div className="test">Hey</div>;
+$('#ron').on('click', () => {
+  if (num === 1) {
+    $('.logo').hide();
+    console.log('hello!');
+  } else {
+    $('.logo2').hide();
+    console.log('EY!');
+  }
+});
+$('#kanye').on('click', () => {
+  if (num === 1) {
+    $('.logo').hide();
+  } else {
+    $('.logo2').hide();
+  }
+});
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-
-import reducers from './reducers';
-
-import App from './components/app';
-
+// axios.get('https://api.tronalddump.io/random/quote', { headers: { accept: 'application/hal+json' } }).then((response) => {
+//   str = JSON.stringify(response.data);
+//   console.log(str);
+//   $('#main2').text(str);
+// });
 
 
 // this creates the store with the reducers, and does some other stuff to initialize devtools
